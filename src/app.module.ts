4e6ce4f0d26c5ba899664cebce87e37584017579
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-10-14 11:07:24 +0530

    crud in NestJs using MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentController } from './controller/student/student.controller';
 import { StudentSchema } from './schema/student.schema';
-
+import { StudentService } from './service/student/student.service';
 @Module({
-  imports: 
-    //The forRoot() method accepts the same configuration object as mongoose.connect() from the Mongoose package
-    [MongooseModule.forRoot('mongodb://localhost:27017/studentdb'),
-      MongooseModule.forFeature([
-        {
-          name: 'Student',
-          schema: StudentSchema
-        }
-      ])
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports:[MongooseModule.forRoot('mongodb://localhost:27017/studentdb'),
+MongooseModule.forFeature([{ name: 'Student', schema: StudentSchema }])],
+  controllers: [AppController,StudentController],
+  providers: [AppService,StudentService],
 })
 export class AppModule {}
